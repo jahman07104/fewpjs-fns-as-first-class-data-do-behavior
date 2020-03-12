f72eedb8dd0bde13ac7becf6a85b1a7f6f426110
Patrick Harrison <jahman07104@gmail.com>
2020-03-12 18:13:30 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+let morning= "12:00"
+let afternoon= between 12:00 and 5:00
+let evening=after 5pm
+function greet() =>{
+  if 
+  time before 12:00
+  return "Good Morning"
+  else
+  if time after 12:00 
+  return "Good Afternoon"
+  else
+  if time after 17:00
+  return "Good Evening"
+}
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+/
+* Write your implementation of displayMessage() */
